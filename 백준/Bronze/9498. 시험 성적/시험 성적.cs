40088c49f3a7e76efcf689703c65fef0059465de
Jonghyeok, Ahn <sraed2@naver.com>
2023-11-18 00:33:13 +0900

    [Bronze V] Title: 시험 성적, Time: 64 ms, Memory: 5496 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.cs
@@ -0,0 +1,22 @@
+int score = Convert.ToInt32(Console.ReadLine());
+
+if(score >= 90)
+{
+    Console.WriteLine("A");
+}
+else if(score >= 80)
+{
+    Console.WriteLine("B");
+}
+else if( score >= 70)
+{
+    Console.WriteLine("C");
+}
+else if( score >= 60)
+{
+    Console.WriteLine("D");
+}
+else
+{
+    Console.WriteLine("F");
+}
