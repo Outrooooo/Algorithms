3ac7ef2a3a9f7160a41366bb627e2dd87b072bc2
Jonghyeok, Ahn <sraed2@naver.com>
2023-11-20 00:41:39 +0900

    [Bronze V] Title: 개, Time: 60 ms, Memory: 5324 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.cs
@@ -0,0 +1,17 @@
+using System.Linq;
+
+namespace TestAlgorithms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(
+                @"|\_/|" + "\n" +
+                @"|q p|   /}" + "\n" +
+                @"( 0 )""""""\" + "\n" +
+                @"|""^""`    |" + "\n" +
+                @"||_/=\\__|");
+        }
+    }
+}
