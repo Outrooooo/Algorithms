17f9c7c8141bf559eae4c899593629f0af1976fb
Jonghyeok, Ahn <sraed2@naver.com>
2023-11-19 23:32:25 +0900

    [Bronze V] Title: A+B, Time: 68 ms, Memory: 5488 KB -BaekjoonHub

--- 백준/Bronze/1000. A＋B/A＋B.cs
+++ 백준/Bronze/1000. A＋B/A＋B.cs
@@ -1,25 +1,3 @@
-using System;
+string[] input = Console.ReadLine().Split(' ');
 
-namespace plus
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            string input;
-            string[] token;
-            int[] data;
-            input = Console.ReadLine();
-            token = input.Split(' ');
-            data = Array.ConvertAll(token, s => int.Parse(s)); // token의 배열값을 전부 정수형으로 변환 후 data 배열에 저장
-
-            Sum(data[0], data[1]);
-
-        }
-
-        static public void Sum(int a, int b)
-        {
-            Console.WriteLine( a + b );
-        }
-    }
-}
+Console.WriteLine(int.Parse(input[0]) + int.Parse(input[1]));
